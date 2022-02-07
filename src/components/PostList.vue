1f507f8374c7bd8b261bba10ef00b6463b9b7673
Gankerjack <sebastiaancrisan@gmail.com>
2022-02-07 17:53:13 +0100

    add lifecycle hooks to setup, add posts toggle, add posts remove

---
+++ src/components/PostList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="post-list"></div>
+  <div v-for="post in posts" :key="post.id">
+    <SinglePost :post="post" />
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from "@vue/runtime-core";
+import SinglePost from "./SinglePost.vue";
+
+export default {
+  components: { SinglePost },
+  props: ["posts"],
+  setup(props) {
+    onMounted(() => {
+      console.log("Mounted");
+    });
+    onUnmounted(() => {
+      console.log("Component unmounted");
+    });
+    onUpdated(() => {
+      console.log("Component updated");
+    });
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>Home</h1>
+    <PostList v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">Toggle posts</button>
+    <button @click="posts.pop()">Delete a post</button>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { ref } from "vue";
+import PostList from "../components/PostList.vue";
 
 export default {
   name: "Home",
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      { title: "Welcome to the blog", body: "Lorem ipsum", id: 1 },
+      { title: "Top 5 css tips", body: "Lorem ipsum", id: 2 },
+    ]);
+    const showPosts = ref(true);
+    return { posts, showPosts };
+  },
 };
 </script>

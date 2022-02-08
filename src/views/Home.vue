6fc589f8990ef64073de7b34b7e5cfac3e29995c
Gankerjack <sebastiaancrisan@gmail.com>
2022-02-08 12:37:30 +0100

    add vue router, add composables, add json-server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts" />
     <button @click="showPosts = !showPosts">Toggle posts</button>
     <button @click="posts.pop()">Delete a post</button>
+    <div class="error" v-if="error">{{ error }}</div>
+    <div class="post-list" v-if="posts.length">
+      <PostList v-if="showPosts" :posts="posts" />
+    </div>
+    <div class="loading" v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
 import PostList from "../components/PostList.vue";
+import getPosts from "../composables/getPosts";
 
 export default {
   name: "Home",
   components: { PostList },
+  //setup hook
   setup() {
-    const posts = ref([
-      { title: "Welcome to the blog", body: "Lorem ipsum", id: 1 },
-      { title: "Top 5 css tips", body: "Lorem ipsum", id: 2 },
-    ]);
-    const showPosts = ref(true);
-    return { posts, showPosts };
+    const { posts, error, showPosts, load } = getPosts();
+    load();
+    return { posts, showPosts, error };
   },
 };
 </script>
